7841e61b661c514559af2c8fa97c450966f031b1
kandersen <kja@cs.au.dk>
2013-09-05 11:03:45 +0200

    Added key to _insert_

--- src/heap.h
+++ src/heap.h
@@ -7,11 +7,12 @@ typedef struct heap heap;
 typedef struct item item;
 
 heap* make_heap    ();
-void  insert       (item* i, heap* h);
+void  insert       (int key, item* i, heap* h);
 item* find_min     (heap* h);
 item* delete_min   (heap* h);
 heap* meld         (heap* h1, heap* h2);
 void  decrease_key (int delta, item* i, heap* h);
 void  delete       (item* i, heap* h);
+
 
 #endif
