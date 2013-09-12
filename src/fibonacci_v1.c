4479d0a6d5157ad9127d0d6f62d282816b84a110
Troels Leth Jensen <troelslethjensen@gmail.com>
2013-09-12 12:30:57 +0200

    Start of to_dot *doesn't compile*

--- src/fibonacci_v1.c
+++ src/fibonacci_v1.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "heap.h"
 
 typedef struct node node;
@@ -56,5 +57,58 @@ heap* meld         (heap* h1, heap* h2) {
 void* delete_min   (heap* h);
 void  decrease_key (int delta, void* i, heap* h);
 void  delete       (void* i, heap* h);
-void  to_dot	   (heap* h);
 
+int counter;
+void  to_dot (heap* h, char* filename) {
+  FILE * out_file = fopen(filename, "w");
+
+  if (out_file == NULL) {
+    fprintf(stderr, "Error, couldn't open file: %s!", filename);
+  }
+  char node_name[10];
+
+  sprintf(node_name, "node%05d", ++counter);
+  fprintf(out_file, node_name);
+
+  for (int i = 0; i < h->size; i++) {
+    dot_node(node_name, h->forest[i]->child, out_file);
+  }
+
+  fclose(out_file);
+}
+
+void dot_node (char* parent, node* child, FILE* out) {
+  node* curr = child;
+  char* node_name;
+  do {
+    sprintf(node_name, "node%05d", ++counter);
+    fprintf(out, "%s", node_name);
+    fprintf(out, "%s --> %s", parent, node_name);
+
+    if (curr->child)
+      dot_node(node_name, curr->child, out);
+
+    curr = child->left_sibling;
+  }
+  while (curr != child)
+}
+
+/* 
+struct node { 
+   int key; 
+   int rank;
+   int marked; 
+   void* value; 
+   node* parent;
+   node* child;
+   node* left_sibling;
+   node* right_sibling;
+ }; 
+
+ 
+struct heap {  
+   node** forest; 
+   int size; 
+   node* min_node; 
+ }; 
+*/
