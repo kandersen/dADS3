3e7ff0229a9df399ddc4c0b239947730e6af3ee1
mkroghj <morten@dotnamics.com>
2013-09-19 14:02:33 +0200

    Closer closer closer

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main() {
   char* file = "test.dot";
   heap* h = make_heap();
 
-  for (int k = 0; k < 13; k++) {
+  for (int k = 0; k < 4; k++) {
     item* i = (item*)malloc(sizeof(item));
     i->key = k;
     i->value = NULL;
@@ -19,14 +19,14 @@ int main() {
   to_dot(h, file);
    item* j;
 
-  do {
+   /*do {
     j = delete_min(h); 
     if (j != NULL) {
       puts("removed one!");
       printf("%i\n", j->key);
     }
   } 
-  while (j != NULL);
-
+  while (j != NULL); */
+ 
   return 0;
 }
