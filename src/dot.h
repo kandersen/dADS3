fced7f99b4ad025f6c967f2c00675f957725be62
Troels Leth Jensen <troelslethjensen@gmail.com>
2013-10-03 22:51:32 +0200

    change layout

--- src/dot.h
+++
@@ -1,8 +0,0 @@
-#ifndef DADS3_DOT_H
-#define DADS3_DOT_H
-
-#include <heap.h>
-
-void  to_dot      (heap* h);
-
-#endif
--- src/tester.c
+++ src/tester.c
@@ -1,9 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "heap.h"
-#include "dijkstra.h"
-#include "graph.h"
-#include <time.h>
+
 
 void test_0(int);
 void test_1(int);
@@ -63,7 +61,7 @@ test_0 (int size) {
     item* i = (item*)malloc(sizeof(item));
     i->key = k;
     i->value = NULL;
-    insert(i, h);
+    insert_item(i, h);
   }
 
   printf("min: %d\n", find_min(h)->key);
@@ -77,7 +75,7 @@ test_1 (int size) {
     item* i = (item*)malloc(sizeof(item));
     i->key = k;
     i->value = NULL;
-    insert(i, h);
+    insert_item(i, h);
   }
   delete_min(h);
 }
@@ -90,11 +88,11 @@ test_2(int size) {
     item* i = (item*)malloc(sizeof(item));
     i->key = k;
     i->value = NULL;
-    insert(i, h);
+    insert_item(i, h);
   }
 
   for (int kk = 0; kk < size; kk++) {
-    item* t = delete_min(h);
+    delete_min(h);
   }
 }
 
@@ -106,7 +104,7 @@ test_3(int size) {
     item* i = (item*)malloc(sizeof(item));
     i->key = k;
     i->value = NULL;
-    insert(i, h);
+    insert_item(i, h);
   }
 
   for (int kk = 0; kk < size/10; kk++) {
@@ -117,7 +115,7 @@ test_3(int size) {
     item* i = (item*)malloc(sizeof(item));
     i->key = k;
     i->value = NULL;
-    insert(i, h);
+    insert_item(i, h);
   }
 
   for (int kk = 0; kk < 2*(size/10); kk++) {
@@ -128,7 +126,7 @@ test_3(int size) {
     item* i = (item*)malloc(sizeof(item));
     i->key = k;
     i->value = NULL;
-    insert(i, h);
+    insert_item(i, h);
   }
   for (int kk = 0; kk < 3*(size/10); kk++) {
     delete_min(h);
@@ -137,7 +135,7 @@ test_3(int size) {
     item* i = (item*)malloc(sizeof(item));
     i->key = k;
     i->value = NULL;
-    insert(i, h);
+    insert_item(i, h);
   }
   for (int kk = 0; kk < 4*(size/10); kk++) {
     delete_min(h);
@@ -155,10 +153,10 @@ test_4(int size) {
     items[k] = i;
     i->key = k;
     i->value = NULL;
-    insert(items[k], h);
+    insert_item(items[k], h);
   }
   for (int kk = 0; kk < size; kk++) {
-    remove(items[size - kk - 1], h);
+    remove_item(items[size - kk - 1], h);
   }
 }
 
@@ -173,13 +171,13 @@ test_5 (int size) {
     items[k] = i;
     i->key = k;
     i->value = NULL;
-    insert(items[k], h);
+    insert_item(items[k], h);
   }
 
   delete_min(h);
 
   for (int kk = 1; kk < size; kk++) {
-    remove(items[kk], h);
+    remove_item(items[kk], h);
   }  
 }
 
@@ -194,13 +192,13 @@ test_6 (int size) {
     items[k] = i;
     i->key = k;
     i->value = NULL;
-    insert(items[k], h);
+    insert_item(items[k], h);
   }
 
   delete_min(h);
 
   for (int kk = 1; kk < size; kk++) {
-    remove(items[size - kk - 1], h);
+    remove_item(items[size - kk - 1], h);
   }  
 }
 
@@ -215,7 +213,7 @@ test_7 (int size) {
     items[k] = i;
     i->key = k;
     i->value = NULL;
-    insert(items[k], h);
+    insert_item(items[k], h);
   }
 
   delete_min(h);
@@ -223,23 +221,24 @@ test_7 (int size) {
   for (int kk = 0; kk < size-1; kk++) {
     int half = size / 2;
     if (kk%2 == 0) {
-      remove(items[(half+(kk/2))], h);
+      remove_item(items[(half+(kk/2))], h);
     } else {
-      remove(items[half-((kk+1)/2)], h);
+      remove_item(items[half-((kk+1)/2)], h);
     }
   }  
 }
 
+/*
 int random_in_range (unsigned int min, unsigned int max)
 {
-  int base_random = rand(); /* in [0, RAND_MAX] */
+  int base_random = rand(); // in [0, RAND_MAX] 
   if (RAND_MAX == base_random) return random_in_range(min, max);
-  /* now guaranteed to be in [0, RAND_MAX) */
+  //now guaranteed to be in [0, RAND_MAX) 
   int range       = max - min,
     remainder   = RAND_MAX % range,
     bucket      = RAND_MAX / range;
-  /* There are range buckets, plus one smaller interval
-     within remainder of RAND_MAX */
+  // There are range buckets, plus one smaller interval
+  //  within remainder of RAND_MAX 
   if (base_random < RAND_MAX - remainder) {
     return min + base_random/bucket;
   } else {
@@ -295,7 +294,7 @@ void consistency(int operations) {
           if (r2 > 0.2) {
             decrease_key(selected_item->key / 2, selected_item, h);
           } else {
-            remove(selected_item, h);
+            remove_item(selected_item, h);
             int index = *((int*)(selected_item->value));
             items[index] = NULL;
             free(selected_item);
@@ -305,3 +304,4 @@ void consistency(int operations) {
     }
   }
 }
+*/
