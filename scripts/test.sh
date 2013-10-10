920dfb13e73a4da93db7935062a4da774e5f5145
mkroghj <morten@dotnamics.com>
2013-10-10 20:17:53 +0200

    updated script to include new test-cases

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@ for (( i = 100; i <= 200000; i = i + 100 ))
 do
     for ex in fh1 fh2 bhp bha
     do
-        for (( t = 0; t <= 3; t++ ))
+        for (( t = 0; t <= 5; t++ ))
         do
             for (( r = 1; r <= 3; r++ ))
             do
