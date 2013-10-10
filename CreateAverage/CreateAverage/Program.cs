c1870031b090fb7004c55178c1d209ab0a981d01
mkroghj <morten@dotnamics.com>
2013-10-10 13:21:23 +0200

    the commit of greatness

--- CreateAverage/CreateAverage/Program.cs
+++ CreateAverage/CreateAverage/Program.cs
@@ -8,12 +8,33 @@ namespace CreateAverage
 {
     class Program
     {
+        enum types
+        {
+            bha = 0,
+            bhp = 1,
+            fh1 = 2,
+            fh2 = 3
+        }
+
         static void Main(string[] args)
+        {
+            //ComputeFile(args);
+            for (int i = 0; i <= 3; i++)
+            {
+                for (int j = 0; j <= 9; j++)
+                {
+                    string[] debugArgs = { "xdjk1.csv", string.Format("{0}_djk1_{1}_real.txt", ((types)i).ToString(), j), j.ToString(), i.ToString() };
+                    ComputeFile(debugArgs);
+                }
+            }
+        }
+
+        private static void ComputeFile(string[] args)
         {
             string inputFile = args[0];
             string outputFile = args[1];
             int prob = int.Parse(args[2]);
-
+            int version = int.Parse(args[3]);
 
             using (StreamWriter writer = new StreamWriter(File.OpenWrite(outputFile)))
             {
@@ -23,23 +44,25 @@ namespace CreateAverage
                 using (System.IO.StreamReader sr = File.OpenText(inputFile))
                 {
                     string line = null;
-                        
-                    while ((line = sr.ReadLine()) != null) {
-                        string[] data = line.Split('\t');
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] data = line.Split(' ');
 
                         int thisProb = int.Parse(data[1]);
-                        if (thisProb == prob)
+                        int thisVersion = int.Parse(data[2]);
+                        if (thisProb == prob && thisVersion == version)
                         {
                             int thisPoint = int.Parse(data[0]);
                             if (thisPoint != lastPoint && lastPoint > 0)
                             {
-                                int edges = lastPoint * (1 - prob / 10);
+                                int edges = (lastPoint * lastPoint) * (1 - prob / 10);
                                 ComputePoint(writer, lastPoint, edges, results);
                                 results.Clear();
                             }
 
                             lastPoint = thisPoint;
-                            results.Add(int.Parse(data[5]));
+                            results.Add(int.Parse(data[4]));
                         }
                     }
                 }
@@ -51,8 +74,11 @@ namespace CreateAverage
 
         private static void ComputePoint(StreamWriter sw, int point, int edges, List<int> results)
         {
-            double avg = (from x in results select x).Average() / ((point + edges) * Math.Log(point, 2));
-            sw.WriteLine(string.Format("({0},{1})", point, avg.ToString("N3").Replace(",",".")));
+        //    double avg = (from x in results select x).Average() / (point * Math.Log(point, 2));
+          //  sw.WriteLine(string.Format("({0},{1})", point, avg.ToString("N3").Replace(",",".")));
+
+            double avg = (from x in results select x).Average();
+            sw.WriteLine(string.Format("({0},{1})", point, ((int)avg).ToString()));
         }
     }
 }
