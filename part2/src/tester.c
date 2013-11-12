34c3de5da09bf0fe82cfe437c0f9320acbc90dae
mkroghj <morten@dotnamics.com>
2013-11-12 09:29:45 +0100

    Updating

--- part2/src/tester.c
+++ part2/src/tester.c
@@ -3,6 +3,8 @@
 #include <time.h>
 #include "heap.h"
 
+typedef struct timespec timespec;
+
 long long cmp_counter;
 
 void test_0(int);
@@ -16,7 +18,24 @@ void test_6(int);
 int universe;
 heap* h;
 
+timespec diff(timespec start, timespec end)
+{
+	timespec temp;
+	if ((end.tv_nsec-start.tv_nsec)<0) {
+		temp.tv_sec = end.tv_sec-start.tv_sec-1;
+		temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+	} else {
+		temp.tv_sec = end.tv_sec-start.tv_sec;
+		temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+	}
+	return temp;
+}
+
 int main(int argc, char* argv[]) {
+
+  timespec time1, time2;
+
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
 
   if (argc < 5) {
     printf("Usage: <testno> <testsize> <universe> <construct_time> \n");
@@ -59,6 +78,13 @@ int main(int argc, char* argv[]) {
     test_6(atoi(argv[2]));
     break;
   }
+
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+
+  timespec elapsedspec = diff(time1, time2);
+
+  printf("Seconds: %ld\n", elapsedspec.tv_sec);
+  printf("nano-seconds: %ld\n", elapsedspec.tv_nsec);
 
   elapsed = clock() - start;
 #ifndef INSTRUMENT
